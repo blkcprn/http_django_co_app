eecfb27324d7131775567437de55c3f40b26ba37
blkcprn <blkcprn@gmail.com>
2024-12-09 11:41:18 +0000

    added base model

---
+++ core/vendors/base/model/model.py
@@ -0,0 +1,64 @@
+from django.db import models
+from typing import (
+    Protocol,
+    Tuple,
+    List,
+    Any,
+)
+
+
+class ProtocolModel(Protocol):
+    """Protocol model."""
+    def is_valid(self, **kwargs: Any) -> Tuple[bool, List[str]]:
+        """Check model item."""
+        ...
+
+
+class BasicModel(ProtocolModel, models.Model):
+    """Basic model."""
+    pass
+
+
+class BaseModel(BasicModel):
+    """Base model."""
+    is_valid = models.BooleanField(
+        default=True,
+    )
+    is_blocked = models.BooleanField(
+        default=False,
+    )
+
+    @property
+    def actual(self) -> bool:
+        """Check valid is True, blocked is False."""
+        return self.is_valid and not self.is_blocked
+
+    @actual.setter
+    def actual(self, val: bool) -> None:
+        """Set valid and blocked."""
+        self.is_valid = val
+        self.is_blocked = not val
+
+    def is_valid(self, **kwargs: Any) -> Tuple[bool, List[str]]:
+        """Check is valid and not blocked."""
+
+        is_valid, messages = True, []
+
+        valid = kwargs.get("is_valid", True)
+        blocked = kwargs.get("is_blocked", True)
+
+        if valid and self.is_valid is False:
+            is_valid = False
+            messages.append("Not valid")
+        if  blocked and self.is_blocked is True:
+            is_valid = False
+            messages.append("Blocked")
+
+        return is_valid, messages
+
+
+
+
+
+
+
---
+++ core/vendors/base/model/queryset.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class BaseQuerySet(models.QuerySet):
+    """Base queryset."""
+    pass
+
