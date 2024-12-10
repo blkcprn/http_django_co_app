7ddb741a0d5e4785b6c8d930108de3af19a5368b
blkcprn <blkcprn@gmail.com>
2024-12-10 12:07:59 +0200

    added account app, with custom user model

--- core/settings/settings.py
+++ core/settings/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # apps
+    "apps.account.apps.AccountConfig",
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+AUTH_USER_MODEL = "account.Account"
